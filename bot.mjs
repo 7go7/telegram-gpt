f2d80a4477b6ee9a57af7a303ac6f22306095547
Andrew <34103554+7go7@users.noreply.github.com>
2023-02-07 20:11:26 +0000

    Update bot.mjs

--- bot.mjs
+++ bot.mjs
@@ -2,6 +2,25 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on('text', msg => msg.reply.text("11111"))
-
 export default bot
+
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
+
+bot.on('/ai', (msg) => {
+    query({"inputs": msg.text.substring(4)}).then((response) => {
+        bot.sendMessage(msg.from.id, JSON.stringify(response));
+    });
+});
+
+bot.start();
